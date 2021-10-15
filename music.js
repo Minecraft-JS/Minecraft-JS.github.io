f3fb92f1c13c494de7059d7b459029323656cff3
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2021-10-15 16:07:08 -0500

    I Dont Where U Are But Ur Dead

--- music.js
+++
@@ -1,5 +0,0 @@
-const music = new Audio('https://cdn.glitch.me/15988d9d-2aef-443b-841f-12be7feda31e%2FSubwoofer%20Lullaby.mp3');
-music.play();
-music.loop =true;
-music.playbackRate = 2;
-music.pause();
